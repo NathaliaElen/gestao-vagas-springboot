c92b98a090d80e6db3ff81cd85b062ddb2adbf40
Nathália Elen <nathaliaelenestudos@gmail.com>
2026-08-01 09:18:18 -0300

    feat: criando DTOs Job

---
+++ src/main/java/br/com/nathaliaelen/gestao_vagas/modules/company/dtos/JobRequestDTO.java
@@ -0,0 +1,19 @@
+package br.com.nathaliaelen.gestao_vagas.modules.company.dtos;
+
+import org.hibernate.validator.constraints.Length;
+
+import br.com.nathaliaelen.gestao_vagas.modules.company.enums.JobLevel;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record JobRequestDTO(
+  @Length(max = 255, message = "O campo [description] deve conter no máximo (255) caracteres.")
+    String description,
+            
+  @NotBlank(message = "O campo [benefits] é obrigatório.")
+  @Length(max = 255, message = "O campo [benefits] deve conter no máximo (255) caracteres.")
+    String benefits,
+          
+  @NotNull(message = "O campo [level] é obrigatório.")
+    JobLevel level
+) {}
---
+++ src/main/java/br/com/nathaliaelen/gestao_vagas/modules/company/dtos/JobResponseDTO.java
@@ -0,0 +1,14 @@
+package br.com.nathaliaelen.gestao_vagas.modules.company.dtos;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import br.com.nathaliaelen.gestao_vagas.modules.company.enums.JobLevel;
+
+public record JobResponseDTO(
+  UUID id,
+  String description,
+  String benefits,
+  JobLevel level,
+  LocalDateTime createAt
+) {}
